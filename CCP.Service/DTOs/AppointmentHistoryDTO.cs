36caba541cf83d154d7889e3bbc27962fc0124f7
Nguyen Ngo <nguyennhm.l.1922@gmail.com>
2025-04-03 11:31:46 +0700

    modify expert appointment

--- CCP.Service/DTOs/AppointmentHistoryDTO.cs
+++ CCP.Service/DTOs/AppointmentHistoryDTO.cs
@@ -10,15 +10,15 @@ namespace CCP.Service.DTOs
     {
         public Guid Id { get; set; }
         public string ParentId { get; set; }
+        public string ParentName { get; set; } // New property for parent's full name
         public Guid ChildId { get; set; }
+        public string ChildName { get; set; }
         public Guid ExpertId { get; set; }
         public TimeSpan StartTime { get; set; }
         public TimeSpan EndTime { get; set; }
         public DateTime BookingDate { get; set; }
         public string Status { get; set; }
         public decimal Price { get; set; }
-        // Optional: Add child name or parent name if needed
-        //public string ChildName { get; set; }
-        //public string ParentName { get; set; }
+       
     }
 }
